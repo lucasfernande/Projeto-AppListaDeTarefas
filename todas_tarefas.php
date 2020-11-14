d59e8adfbe6b234b1e82388fcb3ed296dde1c726
lucasfernande <lucas.fernandes6192@outlook.com>
2020-11-14 16:39:11 -0300

    marcando tarefas como realizadas

--- todas_tarefas.php
+++ todas_tarefas.php
@@ -62,6 +62,10 @@
 				location.href = 'todas_tarefas.php?acao=remover&id='+ id;
 			}
 
+			function tarefaRealizada(id) {
+				location.href = 'todas_tarefas.php?acao=realizada&id='+ id;;
+			}
+
 		</script>
 	</head>
 
@@ -100,8 +104,11 @@
 										</div>
 										<div class="col-sm-3 mt-2 d-flex justify-content-between">
 											<i class="fas fa-trash-alt fa-lg text-danger" onclick="remover(<?= $tarefa->id ?>)"></i>
-											<i class="fas fa-edit fa-lg text-info" onclick="editar(<?= $tarefa->id ?>, '<?= $tarefa->tarefa ?>')"></i>
-											<i class="fas fa-check-square fa-lg text-success"></i>
+
+											<? if ($tarefa->status == 'pendente') { ?>
+												<i class="fas fa-edit fa-lg text-info" onclick="editar(<?= $tarefa->id ?>, '<?= $tarefa->tarefa ?>')"></i>
+												<i class="fas fa-check-square fa-lg text-success" onclick="tarefaRealizada(<?= $tarefa->id ?>)"></i>
+											<? } ?>
 										</div>
 									</div>
 
